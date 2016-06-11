74f3caafd91f9db4909586f92aad9af97c4554c8
Anatoly Mironov <mirontoli@gmail.com>
2016-06-11 19:46:30 +0200

    add users

--- Setup.ps1
+++ Setup.ps1
@@ -1,5 +1,22 @@
 ﻿#sets the two new properties Age and Sex
+[xml]$config = Get-Content .\App.config
 Import-Module -name ".\Modules\SPIntegration.psm1" -Force #-ErrorAction SilentlyContinue -DisableNameChecking
 New-UserProfileProperty -Propertyname "T-Age" -Propertydisplayname "Age" -Description "User Age"
 New-UserProfileProperty -Propertyname "T-Sex" -Propertydisplayname "Sex" -Description "M for Male, F for Female"
 
+function New-Users {
+    $DomainController = $env:COMPUTERNAME
+    $Users = Import-Csv -Delimiter ";" -Path ".Misc\UserList-sn.csv" -Encoding Default  
+    foreach ($User in $Users)            
+    {            
+        $Displayname = $User.Firstname + " " + $User.Lastname            
+        $UserFirstname = $User.Firstname            
+        $UserLastname = $User.Lastname            
+        $OU = $User.OU            
+        $SAM = $User.SAM            
+        $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain            
+        $Description = $User.Description            
+        $Password = $User.Password            
+        New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server $DomainController        
+    }
+}
