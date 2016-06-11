9f3b15a674daa27e013e7a04d643ae8a4cd4cc27
Anatoly Mironov <mirontoli@gmail.com>
2016-06-11 18:59:42 +0200

    Add Setup script for automatic UP Properties creation

--- Modules/SPIntegration.psm1
+++ Modules/SPIntegration.psm1
@@ -34,3 +34,47 @@ function Get-RandomProfile() {
         $global:profiles | Get-Random 
     }
 } 
+
+function New-UserProfileProperty($Propertyname, $Propertydisplayname, $Description) { 
+    asnp *share*
+    $propertylength = 1024
+    $IsUserEditable = $true 
+    $datatype = "string"
+    $ismultivalued = $false
+    #todo: settings
+    $mySiteUrl = "https://dev/my"
+    $site = Get-SPSite $mySiteUrl;
+    $context = Get-SPServiceContext($site);    
+    $upcm = New-Object Microsoft.Office.Server.UserProfiles.UserProfileConfigManager($context);
+    $ppm = $upcm.ProfilePropertyManager;
+    $cpm = $ppm.GetCoreProperties();
+    $ptpm = $ppm.GetProfileTypeProperties([Microsoft.Office.Server.UserProfiles.ProfileType]::User);    
+    $psm = [Microsoft.Office.Server.UserProfiles.ProfileSubTypeManager]::Get($context);
+    $ProfileSubType = [Microsoft.Office.Server.UserProfiles.ProfileSubtypeManager]::GetDefaultProfileName([Microsoft.Office.Server.UserProfiles.ProfileType]::User)
+    $ps = $psm.GetProfileSubtype($ProfileSubType);
+    $pspm = $ps.Properties;
+    $coreProp = $cpm.GetPropertyByName($propertyname);
+    if ($coreProp -eq $null)
+    {
+        $coreProp = $cpm.Create($false);
+        $coreProp.Name = $propertyname;
+        $coreProp.DisplayName = $propertydisplayname;
+        $coreProp.Description = $description;
+        $coreProp.Type = $datatype;
+        $coreProp.IsMultivalued = $ismultivalued;
+        $coreProp.Length = $propertylength;
+        $coreProp.Commit();
+        $profileTypeProp = $ptpm.Create($coreProp);
+        $profileTypeProp.IsVisibleOnEditor = $true;
+        $profileTypeProp.IsVisibleOnViewer = $true;
+        $profileTypeProp.Commit();   
+        $profileSubTypeProp = $pspm.Create($profileTypeProp);
+        $profileSubTypeProp.DefaultPrivacy = [Microsoft.Office.Server.UserProfiles.Privacy]::Public;
+        $profileSubTypeProp.IsUserEditable = $IsUserEditable;
+        $profileSubTypeProp.Commit();
+        #todo $Error Detection
+        Write-Host "User Profile Property $PropertyName has been created" -ForegroundColor Green
+    }else{
+        write-host "User Profile Property $PropertyName already exists." -ForegroundColor DarkGray
+    }
+}
---
+++ Setup.ps1
@@ -0,0 +1,5 @@
+﻿#sets the two new properties Age and Sex
+Import-Module -name ".\Modules\SPIntegration.psm1" -Force #-ErrorAction SilentlyContinue -DisableNameChecking
+New-UserProfileProperty -Propertyname "T-Age" -Propertydisplayname "Age" -Description "User Age"
+New-UserProfileProperty -Propertyname "T-Sex" -Propertydisplayname "Sex" -Description "M for Male, F for Female"
+
