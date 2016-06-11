549eb77a74a1a06bf140b974cfeeea07020c2566
Anatoly Mironov <mirontoli@gmail.com>
2016-06-11 20:01:33 +0200

    add ui module

--- Modules/DataAccessLayer.psm1
+++ Modules/DataAccessLayer.psm1
@@ -82,31 +82,6 @@ function Insert-Match([string]$InstigatorAccountName, [String]$RecipientAccountN
     Run-SQLCmd -Query $Query
 }
 
-function Show-BalloonTip {            
-[cmdletbinding()]            
-param(            
- [parameter(Mandatory=$true)]            
- [string]$Title,            
- [ValidateSet("Info","Warning","Error")]             
- [string]$MessageType = "Info",            
- [parameter(Mandatory=$true)]            
- [string]$Message,            
- [string]$Duration=10000            
-)            
-
-[system.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null            
-$balloon = New-Object System.Windows.Forms.NotifyIcon            
-$path = Get-Process -id $pid | Select-Object -ExpandProperty Path            
-$icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)            
-$balloon.Icon = $icon            
-$balloon.BalloonTipIcon = $MessageType            
-$balloon.BalloonTipText = $Message            
-$balloon.BalloonTipTitle = $Title            
-$balloon.Visible = $true            
-$balloon.ShowBalloonTip($Duration)            
-
-}
-
 <#
 function Get-AllMatchesForPerson($PersonName)
 {
